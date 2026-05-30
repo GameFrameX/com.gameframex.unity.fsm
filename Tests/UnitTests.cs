800fe15addee8edabc651f8d67096c251683a91e
Blank <wangfj11@foxmail.com>
2026-05-30 20:35:01 +0800

    test(fsm): 增加 Reset / AddState / RemoveState 单元测试
    - TestResetFsm: 重置后状态清空、可重新启动
    - TestAddState: 运行时添加状态
    - TestRemoveState: 运行时移除状态
    - TestRemoveCurrentStateThrows: 移除当前运行状态抛异常
    - TestAddDuplicateStateThrows: 添加重复类型抛异常

--- Tests/UnitTests.cs
+++ Tests/UnitTests.cs
@@ -54,7 +54,82 @@ namespace GameFrameX.FSM.Tests
             Assert.IsTrue(m_FsmManager.HasFsm<object>());
         }
 
+        [Test]
+        public void TestResetFsm()
+        {
+            var owner = new object();
+            IFsm<object> fsm = m_FsmManager.CreateFsm(owner, new TestIdleState());
+            fsm.Start<TestIdleState>();
+            Assert.IsTrue(fsm.IsRunning);
+
+            fsm.Reset();
+            Assert.IsFalse(fsm.IsRunning);
+            Assert.IsNull(fsm.CurrentState);
+            Assert.AreEqual(0f, fsm.CurrentStateTime);
+            Assert.IsFalse(fsm.IsDestroyed);
+            Assert.AreEqual(1, fsm.FsmStateCount);
+
+            fsm.Start<TestIdleState>();
+            Assert.IsTrue(fsm.IsRunning);
+            Assert.AreEqual(nameof(TestIdleState), fsm.CurrentState.GetType().Name);
+        }
+
+        [Test]
+        public void TestAddState()
+        {
+            var owner = new object();
+            IFsm<object> fsm = m_FsmManager.CreateFsm(owner, new TestIdleState());
+            Assert.AreEqual(1, fsm.FsmStateCount);
+            Assert.IsFalse(fsm.HasState<TestRunState>());
+
+            fsm.AddState(new TestRunState());
+            Assert.AreEqual(2, fsm.FsmStateCount);
+            Assert.IsTrue(fsm.HasState<TestRunState>());
+
+            fsm.Start<TestRunState>();
+            Assert.AreEqual(nameof(TestRunState), fsm.CurrentState.GetType().Name);
+        }
+
+        [Test]
+        public void TestRemoveState()
+        {
+            var owner = new object();
+            IFsm<object> fsm = m_FsmManager.CreateFsm(owner, new TestIdleState(), new TestRunState());
+            Assert.AreEqual(2, fsm.FsmStateCount);
+
+            bool removed = fsm.RemoveState<TestRunState>();
+            Assert.IsTrue(removed);
+            Assert.AreEqual(1, fsm.FsmStateCount);
+            Assert.IsFalse(fsm.HasState<TestRunState>());
+
+            bool removedAgain = fsm.RemoveState<TestRunState>();
+            Assert.IsFalse(removedAgain);
+        }
+
+        [Test]
+        public void TestRemoveCurrentStateThrows()
+        {
+            var owner = new object();
+            IFsm<object> fsm = m_FsmManager.CreateFsm(owner, new TestIdleState());
+            fsm.Start<TestIdleState>();
+
+            Assert.Throws<GameFrameworkException>(() => fsm.RemoveState<TestIdleState>());
+        }
+
+        [Test]
+        public void TestAddDuplicateStateThrows()
+        {
+            var owner = new object();
+            IFsm<object> fsm = m_FsmManager.CreateFsm(owner, new TestIdleState());
+
+            Assert.Throws<GameFrameworkException>(() => fsm.AddState(new TestIdleState()));
+        }
+
         private class TestIdleState : FsmState<object>
+        {
+        }
+
+        private class TestRunState : FsmState<object>
         {
         }
     }
